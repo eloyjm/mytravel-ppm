358850c0e928fcceb81189b5483f6f3b5edc8e80
eloyjm <eloyjm93@gmail.com>
2023-05-28 19:37:05 +0200

    admin

--- build.sh
+++ build.sh
@@ -5,5 +5,10 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
