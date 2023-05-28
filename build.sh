6a8171debdc18d7721877da22d67968859635929
eloyjm <eloyjm93@gmail.com>
2023-05-28 23:22:56 +0200

    admin done

--- build.sh
+++ build.sh
@@ -5,10 +5,5 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
-
 python manage.py collectstatic --no-input
 python manage.py migrate
